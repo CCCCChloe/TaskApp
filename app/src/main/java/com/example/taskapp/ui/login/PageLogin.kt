c0d20e91b0cb9f0f2fe15e5b583fa3cc51d09cd5
CCCCChloe <tpanchloe@gmail.com>
2020-06-08 23:20:02 -0500

    add login UI

---
+++ app/src/main/java/com/example/taskapp/ui/login/PageLogin.kt
@@ -0,0 +1,33 @@
+package com.example.taskapp.ui.login
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.taskapp.R
+
+class PageLogin : Fragment() {
+
+    companion object {
+        fun newInstance() = PageLogin()
+    }
+
+    private lateinit var viewModel: PageLoginViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.page_login_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(PageLoginViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
